0977b13a2a517c64aa9fa3e91a2bb0a89d3a2a90
RaviKumarRajbhar <thedestroyerreaper@gmail.com>
2024-03-18 23:50:22 +0530

    seekhe gelam

---
+++ CODE CHEF/PROBLEMS/PCL05_problems_FLOW001.c
@@ -0,0 +1,46 @@
+
+// Difficulty:  242
+// Prev
+// Next
+// Add Two Numbers
+// Your task is very simple: given two integers 
+// �
+// A and 
+// �
+// B, write a program to add these two numbers and output the sum.
+
+// Input Format
+// The first line contains an integer 
+// �
+// T, the total number of test cases.
+// Then follow 
+// �
+// T lines, each line contains two integers, 
+// �
+// A and 
+// �
+// B.
+// Output Format
+// For each test case, add 
+// �
+// A and 
+// �
+// B and display the sum in a new line.
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    // Loop for each test case
+    while (t--) {
+        int a, b , s;
+
+        scanf("%d %d", &a, &b);
+        s = a + b ;
+        printf("%d\n",s);
+        return 0;
+        // Your code for each test case goes here 
+    }
+}
